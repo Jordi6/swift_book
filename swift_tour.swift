e8396481d6de9d3035b49973f6f7dc0abe6febd7
jordiB <jordybecerril@gmail.com>
2024-09-05 01:14:52 -0500

    Error Handling

--- swift_tour.swift
+++ swift_tour.swift
@@ -886,9 +886,6 @@ print(protocolValue.simpleDescription)
 
 
 
-
-
-
 // Error Handling
 // you represent errors using any type that adpots the "Error" protocol.
 
@@ -904,6 +901,78 @@ enum PrinterError: Error {
 // If you throw an error in a function, the function returns immediately and the code that called
 // the function handels the error.
 
+// there are several ways to handle errors. One way is to use "do-catch". Inside the do block, you
+// mark code that can throw an error by writing "try" in front of it. Inside the "catch" block,
+// the error is automatically given the name "error" unless you give it a different name. 
+
+
+func send(job: Int, toPrinter printerName: String) throws -> String {
+    if printerName == "Never Has Toner" {
+        throw PrinterError.noToner
+    }
+    return "Job sent"
+}
+
+
+do {
+    let printerResponse = try send(job: 1040, toPrinter: "Bi Sheng")
+    print(printerResponse)
+} catch {
+    print(error)
+}
+// Prints "Job sent"
+
+
+
+// you can privde multiple catch blocks that handle specific errors. You write a pattern after
+// "catch" just as you do after "case" in a switch.
+
+do {
+    let printerResponse = try send(job: 1440, toPrinter: "Gutenberg")
+    print(printerResponse)
+} catch PrinterError.onFire {
+    print("I'll just put this over here, with the rest of the fire.")
+} catch let printerError as PrinterError {
+    print("Printer error: \(printerError).")
+} catch {
+    print(error)
+}
+
+
+
+// another way to handle errors is to use "try?" to convert the result to an optional. If the 
+// function throws an error, the specific error is discarded and the result is nil.
+// Otherwise, the result is an optional containing the value that the function returned.
+
+
+let printerSuccess = try? send(job: 1884, toPrinter: "Mergenthaler")
+let printerFailure = try? send(job: 1885, toPrinter: "Never Has Toner")
+
+
+// use "defer" to write a block of code that's executed after all other code in the function, just
+// before the function returns. The code is executed regardless of whether the function throws an
+// error. You can use "defer" to write setup and cleanup code next to each other, even though they
+// need to be executed at different times.  
+
+
+var fridgeIsOpen = false
+let fridgeContent = ["milk", "eggs", "leftovers"]
+
+func fridgeContains(_ food: String) -> Bool {
+    fridgeIsOpen = true
+    defer {
+        fridgeIsOpen = false
+    }
+
+    let result = fridgeContent.contains(food)
+    return result
+}
+if fridgeContains("banana") {
+    print("Found a banana")
+}
+print(fridgeIsOpen)
+print()
+// Prints "false"
 
 
 
